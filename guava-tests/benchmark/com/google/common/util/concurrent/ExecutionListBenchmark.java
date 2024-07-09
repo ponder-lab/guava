ee788784399301aae0e610270ff9abd98330ea0b
Benjamin Prud'homme <139664105+bgprudhomme@users.noreply.github.com>
2024-07-09 13:20:21 -0400

    Update ExecutionListBenchmark.java

--- guava-tests/benchmark/com/google/common/util/concurrent/ExecutionListBenchmark.java
+++ guava-tests/benchmark/com/google/common/util/concurrent/ExecutionListBenchmark.java
@@ -36,6 +36,7 @@ import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -227,7 +228,7 @@ public class ExecutionListBenchmark {
     abstract ExecutionListWrapper newExecutionList();
   }
 
-  private ThreadPoolExecutor executorService;
+  private ExecutorService executorService;
   private CountDownLatch listenerLatch;
   private ExecutionListWrapper list;
 
@@ -247,13 +248,7 @@ public class ExecutionListBenchmark {
   @BeforeExperiment
   void setUp() throws Exception {
     executorService =
-        new ThreadPoolExecutor(
-            NUM_THREADS,
-            NUM_THREADS,
-            Long.MAX_VALUE,
-            TimeUnit.SECONDS,
-            new ArrayBlockingQueue<Runnable>(1000));
-    executorService.prestartAllCoreThreads();
+        Executors.newVirtualThreadPerTaskExecutor();
     final AtomicInteger integer = new AtomicInteger();
     // Execute a bunch of tasks to ensure that our threads are allocated and hot
     for (int i = 0; i < NUM_THREADS * 10; i++) {
